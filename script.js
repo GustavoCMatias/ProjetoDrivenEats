e48a4ac672819e970bdfe283835499968319836b
GustavoCMatias <gustavocmatias@gmail.com>
2022-10-16 18:47:25 -0300

    Preços salvos, rodape modificado quando selecionado

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+let preco_prato;
+let preco_bebida;
+let preco_sobremesa;
+
 function escolher(grupo, item) {
     const elementos = document.querySelectorAll(".item");
     const icone = document.querySelectorAll(".icone");
@@ -8,5 +13,33 @@ function escolher(grupo, item) {
 
     elementos[5 * grupo + item - 6].classList.add("item-borda");
     icone[5 * grupo + item - 6].classList.remove("escondido");
+
+    const html = elementos[5 * grupo + item - 6].innerHTML;
+    const loc = html.search('<h6>');
+    if (grupo === 1) {
+        preco_prato = Number(`${html.substring(loc + 7, loc + 9)}.${html.substring(loc + 10, loc + 12)}`);
+    } else if (grupo === 2) {
+        preco_bebida = Number(`${html.substring(loc + 7, loc + 9)}.${html.substring(loc + 10, loc + 12)}`);
+    } else {
+        preco_sobremesa = Number(`${html.substring(loc + 7, loc + 9)}.${html.substring(loc + 10, loc + 12)}`);
+    }
+
+    if (preco_prato && preco_bebida && preco_sobremesa) {
+        const novoele = document.querySelector(".text-rodape");
+        novoele.classList.remove("text-rodape");
+        novoele.classList.add("rodape-green-bg")
+
+        novoele.innerHTML = "<p>Fechar pedido!</p>";
+    }
+    /*
+    if (!total_grupo.includes(grupo)) {
+        total_grupo.push(grupo);
+        if (total_grupo.length === 3) {
+            const rodape = document.querySelectorAll(".rodape div");
+            rodape.classList.add("rodape-green-bg")
+        }
+
+    }
+    */
 }
 
